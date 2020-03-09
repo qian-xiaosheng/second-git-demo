46901e541f5eaadab190566e0c94ab658910ba0a
qian-xiaosheng <1326598206@qq.com>
2020-03-09 16:35:47 +0800

    提交测试的git项目

---
+++ src/main/java/com/yida/secondgitdemo/SecondGitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.yida.secondgitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondGitDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondGitDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yida/secondgitdemo/SecondGitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yida.secondgitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondGitDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
