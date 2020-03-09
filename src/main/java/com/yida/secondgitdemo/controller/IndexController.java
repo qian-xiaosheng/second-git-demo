a8596c436b73da77bc3c906c1d48d5fa01702ef1
qian-xiaosheng <1326598206@qq.com>
2020-03-09 17:16:25 +0800

    添加一个vc

---
+++ src/main/java/com/yida/secondgitdemo/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.yida.secondgitdemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    @GetMapping({"/index","","/"})
+    public String index() {
+        return "hello world";
+    }
+}
